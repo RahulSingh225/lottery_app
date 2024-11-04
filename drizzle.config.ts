29a104931fe3cc8dd9ac1b5aada223297f009a20
Rahul Singh <42008021+RahulSingh225@users.noreply.github.com>
2024-11-04 11:49:28 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://apollodbadmin:Test@123@35.207.195.181:5432/postgres",
+    url: 
   },
 });
